7cff94552ad4f0aca1618e8676d3e9d7c0c00c9d
Alex <alexss81@mail.ru>
2023-11-03 21:35:08 +0300

    testing ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus.test.js
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.js
@@ -14,23 +14,41 @@ describe("ProfileStatus Component", () => {
     test("after creation <span> with status should be displayed",async () => {
       const component = create(<ProfileStatus status="it-kamasutra.com" />);
       const root = component.root;
-      let span = await root.findByType("span");
+      let span = await  root.findByType("span");
       expect(span).not.toBeNull();
       });
 
-    test("after creation <inpit>  shouldn't be displayed",() => {
+    test("after creation <inpit>  shouldn't be displayed",async() => {
       const component = create(<ProfileStatus status="it-kamasutra.com" />);
       const root = component.root;
-      expect(async() => {
-            await root.findByType("input");
+      expect(() => {
+           await root.findByType("input");
       }).toThrow();
       });
-  
 
-    test("after creation <span>  should contains correct status", async () => {
+    test("after creation <span>  should contains correct status",async  () => {
         const component = create(<ProfileStatus status="it-kamasutra.com" />);
         const root = component.root;
-        let span = await root.findByType("span");
+        let span =  await root.findByType("span");
         expect(span.children[0]).toBe("it-kamasutra.com");
         });
+
+    test("input should be displayed in editMode instead of span", async () => {
+      const component = create(<ProfileStatus status="it-kamasutra.com" />);
+      const root = component.root;
+      let span = await root.findByType("span");
+      span.props.onDoubleClick()
+      let input = await root.findByType("input")
+      expect(input.props.value).toBe("it-kamasutra.com");
+      }); 
+
+      test("callback should be called", () => {
+        const mockCallback = jest.fn();
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatus = {mockCallback} />);
+        const instance = component.getInstance();
+        instance.deactivateEditMode();
+
+        expect(mockCallback.mock.calls.length).toBe(1);
+        });   
+
     });
